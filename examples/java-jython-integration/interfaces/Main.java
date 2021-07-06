ff6fde9e45fae9da39ecfd14443b9e11fa299654
Rickay <66440980+Rickaym@users.noreply.github.com>
2021-07-06 18:51:59 +0630

    Delete examples directory

--- examples/java-jython-integration/interfaces/Main.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public class Main {
-    public static void main(String[] args) {
-        HumanFactory factory = new HumanFactory();
-
-        factory.createMale("Ricky", "12");
-    }
-}
