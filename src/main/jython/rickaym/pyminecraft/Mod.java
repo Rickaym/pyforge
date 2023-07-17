6733b975476d8391689ea7aebc42898a69ffb2d4
Ricky <66440980+Rickaym@users.noreply.github.com>
2023-07-17 23:08:45 +0630

    rewrite Mod.py decorator in pure java

---
+++ src/main/jython/rickaym/pyminecraft/Mod.java
@@ -0,0 +1,32 @@
+package rickaym.pyminecraft;
+
+import org.python.core.*;
+
+/** Class decorator to mark as a mod. */
+public class Mod extends PyObject {
+    public static String mod_id;
+
+    @Override
+    public PyObject __call__(PyObject[] args, String[] keywords) {
+        PyObject mod_class = args[0];
+        {
+            if (!(mod_class instanceof PyClass)) {
+                throw Py.TypeError("Expected class type, got " + mod_class.getType()
+                        .fastGetName());
+            }
+            class ModMetaFunction extends PyObject {
+                @Override
+                public PyObject __call__() {
+                    return new PyTuple(
+                            new PyTuple(new PyString("mod_id"), new PyString(mod_id)),
+                            new PyTuple(new PyString("class_name"), new PyString(mod_class.getType()
+                                    .fastGetName()))
+                    );
+                }
+            }
+            mod_class.__setattr__("__mod_meta__", new ModMetaFunction());
+            return mod_class;
+        }
+    }
+}
+
