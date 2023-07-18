f2201d91615f362598dabcfb7657c3d14280de50
Ricky <66440980+Rickaym@users.noreply.github.com>
2023-07-18 21:36:29 +0630

    remove path

--- src/main/jython/tests/Main.java
+++ src/main/jython/tests/Main.java
@@ -8,8 +8,6 @@ import java.nio.file.Paths;
 
 
 public class Main {
-    static String package_path = "C:/Users/User/Documents/Programming/Java/far_away_package";
-
     public static void main(String[] args) {
         PyModLoader.loadMod(Paths.get("src")
                 .toAbsolutePath()
