396fac1e490b3d9ebfe67679e2f6b1d5ea471e37
Ricky <66440980+Rickaym@users.noreply.github.com>
2023-07-21 20:37:54 +0630

    interpreter vs system state test

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -4,35 +4,36 @@ import org.python.core.Py;
 import org.python.core.PyObject;
 import org.python.core.PySystemState;
 import org.python.util.PythonInterpreter;
+import rickaym.pyminecraft.PyModLoader;
+
+import java.nio.file.Paths;
 
 
 public class Main {
     public static void main(String[] args) {
-        String modEntryFilePath = "";
-        PyObject module;
-        String[] descPath = modEntryFilePath.replace("\\", "/")
-                .split("/");
-
-        String moduleName = descPath[descPath.length - 2];
-
+        String moduleName = "examplemod";
+        String sourceDir = Paths.get("src")
+                .toAbsolutePath()
+                .normalize()
+                .toString();
+        PyModLoader.loadMod(sourceDir, moduleName);
     }
-    private void testLoadInterpreter(String sourceDir, String moduleName) {
+    private static void testLoadInterpreter(String sourceDir, String moduleName) {
         PythonInterpreter jyInterpreter = new PythonInterpreter();
         jyInterpreter.exec("import sys");
         jyInterpreter.exec(String.format("sys.path.append(\"%s\")", sourceDir));  // replace with actual path
         jyInterpreter.exec(String.format("import %s", moduleName));  // replace with actual module name
         PyObject module = jyInterpreter.get(moduleName);
-        System.out.format("Loaded in module '%s'.\n", module);
+        System.out.format("Interpreter loaded in module '%s'.\n", module);
     }
 
-    private void testLoadSysState(String sourceDir, String moduleName) {
+    private static void testLoadSysState(String sourceDir, String moduleName) {
         PySystemState sys = new PySystemState();
-        sys.path.append(Py.newString(sourceDir.replace("\\", "\\\\")));
-        // Print the Python path after adding the directory
+        sys.path.append(Py.newString(sourceDir));
         PyObject importer = sys.getBuiltins()
                 .__getitem__(Py.newString("__import__"));
         System.out.format("%s importer calling the top level module '%s'.\n", importer, moduleName);
         PyObject module = importer.__call__(Py.newString(moduleName));
-
+        System.out.format("System state loaded in module '%s'.\n", module);
     }
 }
